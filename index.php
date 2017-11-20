1853bd9612910ea9e83c9dc9efd0de446430a795
Roman <roman.lossin@icloud.com>
2017-11-20 08:28:43 +0100

    ADD: Added PSR1/2 code styling

--- index.php
+++ index.php
@@ -1,32 +1,25 @@
 <?php
 
 return [
-	'name' => 'metadesc',
-	
-	'type' => 'extension',
-	
-	'main' => function( $app ) {
-	
-	},
-	
-	'events' => [
-		
-		'site' => function( $event, $app ) {
-			
-			$app->on(
-				'view.meta',
-				function( $event, $meta ) use ( $app ) {
-					
-					$meta(
-						[
-							'description' => $meta->get( 'og:description' )
-						]
-					);
-				},
-				-1
-			);
-			
-		}
-	]
+    'name' => 'metadesc',
+    
+    'type' => 'extension',
+    
+    'main' => function ($app) {
+    },
+    
+    'events' => [
+        
+        'site' => function ($event, $app) {
+            
+            $app->on('view.meta', function ($event, $meta) use ($app) {
+                
+                $meta([
+                        'description' => $meta->get('og:description'),
+                    ]);
+            }, -1);
+            
+        },
+    ],
 
 ];
